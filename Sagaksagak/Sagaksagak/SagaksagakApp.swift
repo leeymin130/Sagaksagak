a81185ad96f66eede2a2f223e773750fcd24fc1f
yoomin <leeymin130@gmail.com>
2024-08-15 15:42:45 +0900

    project 초기 셋팅
    1. 깃헙에서 리포지토리를 생성
    2. 필요한 .gitignore 파일을 추가
    3. develop branch 생성
    4. main과 develop branch의 rule 생성
    5. loacal에 클론해서 클론한 폴더에 새로운 xcode 프로젝트 생성~

---
+++ Sagaksagak/Sagaksagak/SagaksagakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SagaksagakApp.swift
+//  Sagaksagak
+//
+//  Created by yoomin on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SagaksagakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
