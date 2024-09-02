7d9a4c3b33a86cde065250095e436fc1c322a19c
yoomin <leeymin130@gmail.com>
2024-09-02 23:17:02 +0900

    [#1] feat : DataModel 작성
    Diary에 필요한 프로퍼티
    :id,imageName,title,comment,board,date,count,favorite,isHidden

---
+++ Sagaksagak/Sagaksagak/Model/Diary.swift
@@ -0,0 +1,17 @@
+//
+//  Diary.swift
+//  Sagaksagak
+//
+//  Created by yoomin on 8/22/24.
+//
+
+import SwiftUI
+
+// id,date,imageName,board,title,comment,count
+struct Diary: Identifiable {
+    let id: UUID().uuidString
+    let date: Date
+    
+}
+
+
